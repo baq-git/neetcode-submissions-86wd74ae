587526cdd50df2dfb23a0824a9bd8db1a851a5ea
baq-git <93764064+baq-git@users.noreply.github.com>
2026-06-06 12:04:39 +0700

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        int point = 0;
+        while (true) {
+            slow = nums[slow];
+            point = nums[point];
+
+            if (slow == point) {
+                return slow;
+            }
+        }
+    }
+}
